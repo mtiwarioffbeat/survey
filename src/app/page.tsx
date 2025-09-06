5438cc0abebe0efbb98920e58ba8c6189290408a
Amit Jain <ajain@offbeatsoftwaresolutions.com>
2025-09-06 13:03:53 +0530

    name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import { useAppSelector, useAppDispatch } from '@/hooks/reduxhooks'
 const page = () => {
    const {text} = useAppSelector((state) => state.auth)
   return (
-    <div className='text-3xl text-red'>{text}</div>
+    <div className='text-3xl text-red'>{text}
+    <h1>hello </h1>
+    </div>
   )
 }
 
