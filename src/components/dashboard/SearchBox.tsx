325adadb2072858b69f48ce263da00ce7becbfb7
Amit Jain <ajain@offbeatsoftwaresolutions.com>
2025-10-01 17:04:03 +0530

    delete preview page

--- src/components/dashboard/SearchBox.tsx
+++ src/components/dashboard/SearchBox.tsx
@@ -1,46 +1,5 @@
-// "use client";
 
-// import React, { useRef } from "react";
-// import { useAppDispatch, useAppSelector } from "@/hooks/reduxhooks";
-// import { setSearchValue } from "@/redux/DashboardSlice/DashboardSlice";
-// import { CiSearch } from "react-icons/ci";
-
-// export default function SearchBox() {
-//   const dispatch = useAppDispatch();
-//   const { searchValue } = useAppSelector((store) => store.dashboard);
-//   const debounceTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const val = e.target.value;
-
-//     // Clear previous timer if it exists
-//     if (debounceTimer.current) {
-//       clearTimeout(debounceTimer.current);
-//     }
-//     // Set a new debounce timer
-//     debounceTimer.current = setTimeout(() => {
-//       dispatch(setSearchValue(val)); // Debounced dispatch
-//       console.log("Dispatched value:", val);
-//     }, 300); // 300ms debounce delay
-//   };
-
-//   return (
-//     <div className="relative max-w-sm">
-//       <input
-//         type="text"
-//         defaultValue={searchValue}
-//         onChange={handleChange}
-//         placeholder="Search... form"
-//         className="w-full pl-10 pr-3 py-1 text-sm rounded-full border border-gray-300 focus:outline-none focus:shadow"
-//       />
-//       <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-//         <CiSearch className="text-xl" />
-//       </div>
-//     </div>
-//   );
-// }
 "use client";
-
 import React from "react";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxhooks";
 import { setSearchValue } from "@/redux/DashboardSlice/DashboardSlice";
